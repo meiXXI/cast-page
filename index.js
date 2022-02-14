0ea479b31bbdc7c69f1c8471599543ebe46d9e5d
meiXXI <11806828+meiXXI@users.noreply.github.com>
2022-02-14 16:26:36 +0000

    Deploying to gh-pages from @ meiXXI/cast-page@ffe5a515ff0102dfef07e3c6730137e129b21e39 🚀

--- index.js
+++ index.js
@@ -19,10 +19,26 @@ connect = function () {
  * Init cast API
  */
 initializeCastApi = function () {
-    cast.framework.CastContext.getInstance().setOptions({
-        receiverApplicationId: applicationId,
-        autoJoinPolicy: chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED
-    });
+    // cast.framework.CastContext.getInstance().setOptions({
+    //     receiverApplicationId: applicationId,
+    //     autoJoinPolicy: chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED
+    // });
 
     console.log("cast api has been initialized...");
+
+    console.log("initinig cast api");
+    var sessionRequest = new chrome.cast.SessionRequest(applicationId);
+    var apiConfig = new chrome.cast.ApiConfig(
+        sessionRequest,
+        _session => {
+            session = _session;
+            log("has config", session);
+        },
+        receiver => console.log("has receiver", receiver)
+    );
+    chrome.cast.initialize(
+        apiConfig,
+        () => console.log("cast init success"),
+        err => console.log("cast init error", err)
+    );
 };
