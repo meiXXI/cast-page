f80d133eb6ff6ace4aba126925a5ea5d275d34df
meiXXI <11806828+meiXXI@users.noreply.github.com>
2022-02-14 16:10:21 +0000

    Deploying to gh-pages from @ meiXXI/cast-page@84330ffc8b607f714f86cb7f8ec29822f9290445 🚀

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 $(document).ready(function () {
 
+    // bind buttons
     $("#btnConnect").bind("click", connect);
 });
+
+const applicationId = "A4136689";
 
 /**
  * Connect to chrome cast
  */
-connect = function() {
+connect = function () {
     alert("xxxx");
 }
+
+/**
+ * Init cast API
+ */
+initializeCastApi = function () {
+    cast.framework.CastContext.getInstance().setOptions({
+        receiverApplicationId: applicationId,
+        autoJoinPolicy: chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED
+    });
+};
